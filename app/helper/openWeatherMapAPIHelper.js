fd6871b63da57958ec8df8c7cd5b412699ecfdbd
rohmanhakim <rohmanhakim@live.com>
2016-09-10 18:53:29 +0700

    Added OpenWeateherMap API helpers

---
+++ app/helper/openWeatherMapAPIHelper.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+//api.openweathermap.org/data/2.5/forecast?q=las vegas,nevada&mode=json&APPID=7f264597e2aac98a65137abcdee73a3f
+const APPID = "7f264597e2aac98a65137abcdee73a3f";
+const BASEURL = "http://api.openweathermap.org/data/2.5";
+const URI_FORECAST = "/forecast?";
+const PARAM_QUERY = "q=";
+const PARAM_MODE = "mode=";
+const PARAM_APPID = "APPID="
+
+function getFiveDaysForecastRequest(place) {
+  return axios.get(BASEURL + URI_FORECAST + PARAM_QUERY + place + "&" + PARAM_MODE + "&" + PARAM_APPID + APPID)
+    .then(function (response) {
+      return response;
+    })
+    .catch(function (error) {
+      console.log("Error in getFiveDaysForecast : ",error);
+    });
+}
+
+var openWeatherMapAPIHelper = {
+  getFiveDaysForecast: function (place) {
+    return getFiveDaysForecastRequest(place);
+  }
+}
+
+module.exports = openWeatherMapAPIHelper;
