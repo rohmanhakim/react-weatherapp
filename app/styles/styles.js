defc815be6a287ceae86d88debc2182883b0d22a
rohmanhakim <rohmanhakim@live.com>
2016-09-10 22:35:55 +0700

    Added styles for forecast component

--- app/styles/styles.js
+++ app/styles/styles.js
@@ -68,6 +68,99 @@ var styles = {
     animationDelay: '-0.16s',
   },
 
+  fiveDaysForecastContainer: {
+      background: 'rgba(255, 255, 255, 0.2)',
+      position: 'fixed',
+      bottom: '100px',
+      left: 0,
+      right: 0,
+      height: '200px'
+  },
+
+  forecastItem: {
+    paddingTop: '20px',
+    height: 'inherit',
+    color: '#fff'
+  },
+
+  forecastIcon: {
+    fontSize: '96px'
+  },
+
+  forecastTextTemp: {
+    fontSize: '27px'
+  },
+
+  dateContainer: {
+    color: '#fff',
+    position: 'relative',
+    padding: '20px',
+    fontSize: '36px',
+    margin: 'auto'
+  },
+
+  placeContainer: {
+    color: '#fff',
+    position: 'relative',
+    padding: '20px',
+    fontSize: '64px',
+    margin: 'auto'
+  },
+
+  todayWeatherIconContainer: {
+    color: '#fff',
+    fontSize: '400px',
+    textAlign: 'center'
+  },
+
+  todayWeatherIcon: {
+    verticalAlign: 'middle'
+  },
+
+  todayWeatherContainer: {
+    position: 'fixed',
+    bottom: '300px',
+    width: '480px',
+    padding: '20px',
+    color: '#fff'
+  },
+
+  todayWeatherTemp: {
+    fontSize: '80px',
+    display: 'inline-block'
+  },
+
+  todayWeatherMetric: {
+    fontSize: '18px',
+    display: 'inline-block',
+    position: 'relative',
+    bottom: '38px'
+  },
+
+  todayWeatherDesc: {
+    fontSize: '24px',
+  },
+
+  todayWeatherMinmaxContainer: {
+    fontSize: '18px',
+    borderBottom: '1px solid #fff',
+    marginBottom: '10px'
+  },
+
+  todayWeatherMin: {
+    display: 'inline-block'
+  },
+
+  todayWeatherAdditionalInfoContainer: {
+    fontSize: '18px',
+    margin: 'auto'
+  },
+
+  todayWeatherMax: {
+    display: 'inline-block',
+    marginLeft: '20px'
+  }
+
 
 
 };
