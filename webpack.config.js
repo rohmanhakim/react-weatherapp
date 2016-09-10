280509f22e586a277dbe947d69f16ea687851cfd
rohmanhakim <rohmanhakim@live.com>
2016-09-10 22:34:22 +0700

    Configured webpack to extract css

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
-})
+});
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   devtool: 'source-map',
@@ -17,9 +18,11 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader:"babel-loader"},
-      {test: /\.css$/, loader:"style-loader!css-loader"},
-      {test: /\.(jpg|png)$/, loader: "url-loader?limit=25000"}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
+      {test: /\.(jpg|png)$/, loader: "url-loader?limit=25000"},
+      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [HtmlWebpackPluginConfig, new ExtractTextPlugin("[name].css",{allChunks: false})]
 }
