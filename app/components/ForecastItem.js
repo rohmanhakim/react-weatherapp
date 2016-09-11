5401ca96e1c2c424bda3c5fa9184470634c7dc43
rohmanhakim <rohmanhakim@live.com>
2016-09-11 17:03:30 +0700

    Enabled forecast item to change icon dynamically

--- app/components/ForecastItem.js
+++ app/components/ForecastItem.js
@@ -5,7 +5,7 @@ const styles = require('../styles/styles');
 function ForecastItem (props) {
   return (
     <div className="col-sm-2 text-center" style={styles.forecastItem}>
-      <i className="owf owf-803" style={styles.forecastIcon}></i>
+      <i className={"owf owf-" + props.forecastItemData[0].weatherId} style={styles.forecastIcon}></i>
       <p>{props.dayOfWeek}</p>
       <p style={styles.forecastTextTemp}>{props.forecastItemData[0].temp}&deg;C</p>
     </div>
